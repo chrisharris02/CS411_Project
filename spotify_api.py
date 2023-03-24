5741c398675812e2c14b0b2aff8667e43b2a9e5a
Chris Harris <chris@incisio.com>
2023-03-24 09:55:46 -0400

    Update spotify_api.py

--- spotify_api.py
+++ spotify_api.py
@@ -4,11 +4,17 @@ import os
 import spotipy
 import sys
 from spotipy.oauth2 import SpotifyClientCredentials
+from dotenv import load_dotenv
+
+load_dotenv()
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'
+TOP_ARTISTS = 'https://api.spotify.com/v1/me/top/artists/'
 
-CLIENT_ID = os.getenv('CLIENT_ID')
-CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
+
+
 # POST
 auth_response = requests.post(AUTH_URL, {
     'grant_type': 'client_credentials',
@@ -20,7 +26,14 @@ auth_response = requests.post(AUTH_URL, {
 auth_response_data = auth_response.json()
 
 # save the access token
-#access_token = auth_response_data['access_token']
+access_token = auth_response_data['access_token']
+
+headers = {
+    'Authorization': 'Bearer {token}'.format(token=access_token)
+}
+
+top_response = requests.get(TOP_ARTISTS, headers=headers)
 
 
-print(auth_response_data)
+
+print(top_response.json())
