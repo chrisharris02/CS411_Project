385fc5a46180e0e9deaf1acff747139b3120d6b7
Chris Harris <chris@incisio.com>
2023-04-24 13:03:35 -0400

    Added redis session management implementation

--- flask-server/config.py
+++ flask-server/config.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+import redis
 load_dotenv()
 
 class ApplicationConfig:
@@ -7,4 +8,7 @@ class ApplicationConfig:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = "sqlite:///sqlite.db"
-    load_dotenv()
+    SESSION_TYPE = "redis"
+    SESSION_PERMANENT = False
+    SESSION_USE_SIGNER = True
+    SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,15 +1,17 @@
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, session
 from flask_bcrypt import Bcrypt
 import os
 import openai
 from dotenv import load_dotenv
 from models import db, User
 from config import ApplicationConfig
+from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(ApplicationConfig)
 
 bcrypt = Bcrypt(app)
+server_session = Session(app)
 db.init_app(app)
  
 with app.app_context():
@@ -36,5 +38,37 @@ def register_user():
         "email": new_user.email
     })
 
+
+@app.route("/@me") #return info about current logged in user
+def get_current_user():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}),401
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+    "id": user.id,
+    "email": user.email
+})
+
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"error": "Unauthorized"}),401
+    
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}),401
+    
+    session["user_id"] = user.id
+
+    return jsonify({
+    "id": user.id,
+    "email": user.email
+})
 if __name__ == "__main__":
     app.run(port=4500,debug=True)
